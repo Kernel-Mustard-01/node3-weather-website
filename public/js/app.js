95afbd5ed5a73dce0a47fb31ab25b50c340a7f5f
theKernel <tf875427@gmail.com>
2021-04-13 14:44:32 +0200

    heroku add

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', e => {
         return console.log('Please choose a location!')
     } else {
 
-        fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+        fetch(`/weather?address=${location}`).then(response => {
             response.json().then(data => {
             if (data.error) {
                 console.log(data.error)
